d7a510da5c3905005f81fdcd2b34ef7a9054e559
nithin <dvznithin@gmail.com>
2024-04-19 12:09:50 +0530

    sixthcommit

--- customer/views.py
+++ customer/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render,redirect
 from . models import *
 from seller.models import Products
+from django.http import HttpResponse
 # Create your views here.
 def index (request):
     return render(request,'customer/index.html')
